f53cd9fec06503f407e10ea86ad7a670e55c9448
Agata <agata.weltrowska@outlook.com>
2019-06-05 20:21:12 +0200

    zadanie 3 - cd

---
+++ src/main/java/com/javagda25/comparator/zadanie3/DruzynaPilkarska.java
@@ -0,0 +1,10 @@
+package com.javagda25.comparator.zadanie3;
+
+public class DruzynaPilkarska {
+    private int bramki_stracone;
+    private int bramki_trafione;
+    private int mecze_wygrane;
+    private int mecze_przegrane;
+    private int mecze_zremisowane;
+    private int liczba_punktow;
+}
---
+++ src/main/java/com/javagda25/comparator/zadanie3/Main.java
@@ -0,0 +1,24 @@
+package com.javagda25.comparator.zadanie3;
+
+// Stwórz komparator porównujący drużyny piłkarskie zgodnie z zasadami FIFA. Tj.
+// Pola klasy DrużynaPiłkarska:
+// bramki stracone,
+// bramki trafione,
+// mecze wygrane,
+// mecze przegrane,
+// mecze zremisowane,
+// liczba punktów
+// - kryterium punktów (im więcej tym lepiej)
+// - kryterium bramek trafionych (im więcej tym lepiej)
+// - kryterium bramek straconych (im mniej tym lepiej)
+// - kryterium ... nazwy drużyny - alfabetycznie sortuje i wygrywa drużyna której nazwa (sortując alfabetycznie)
+// byłaby pierwsza.
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<DruzynaPilkarska> druzynaPilkarskas = new ArrayList<>();
+    }
+}
